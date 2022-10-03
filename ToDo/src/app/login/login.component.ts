7db56c6779631f533195bf496e582b9a9c0f3c50
Nguyen-Dinh-Sang <nguyendinhsang9199@gmail.com>
2022-10-03 16:39:19 +0700

    Components_SangND: Event Binding.

--- ToDo/src/app/login/login.component.ts
+++ ToDo/src/app/login/login.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  username = 'sangnd2';
+  password = '123';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  handleLogin() {
+    console.log(this.username);
+  }
 }
